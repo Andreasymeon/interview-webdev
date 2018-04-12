4d3a95bffcc0eb5c29156a992f1b3915b227621f
Andreasymeon <andreasymeon@me.com>
2018-04-12 10:39:49 +0200

    rectifications

--- js/nav.js
+++ js/nav.js
@@ -3,12 +3,12 @@ function openNav() {
   document.getElementById("sidenav").style.width = "250px";
   document.body.style.marginLeft = "250px";
   document.body.style.paddingLeft = "10";
-  document.getElementById("burgermenu").style.visibility = 'hidden';
+  document.getElementById("burgermenu").style.visibility = "hidden";
 }
 
 /*barre de navigation width and left margin when clicked close*/
 function closeNav() {
   document.getElementById("sidenav").style.width = "0";
   document.body.style.marginLeft = "0";
-  document.getElementById("burgermenu").style.visibility = 'block';
+  document.getElementById("burgermenu").style.visibility = "visible";
 }
